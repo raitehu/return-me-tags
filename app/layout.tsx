8afe79a67da225827da3199a55b14185ce66b00e
SHIMOTORI Masanari <shimotori.masanari@gmail.com>
2025-10-03 00:43:53 +0900

    :test_tube: GTMではなくGAを導入

--- app/layout.tsx
+++ app/layout.tsx
@@ -68,24 +68,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* Google Tag Manager */}
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-T4VQ6F5G45" />
         <script
           dangerouslySetInnerHTML={{
-            __html:
-              "(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer','GTM-NS2ZHBQ7');",
+            __html: `window.dataLayer = window.dataLayer || [];\nfunction gtag(){dataLayer.push(arguments);}\ngtag('js', new Date());\n\ngtag('config', 'G-T4VQ6F5G45');`,
           }}
         />
-        {/* End Google Tag Manager */}
       </head>
       <body className={`${notoSerifJp.className} ${notoSansJp.variable} ${sawarabiMincho.variable}`}>
-        {/* Google Tag Manager (noscript) */}
-        <noscript
-          dangerouslySetInnerHTML={{
-            __html:
-              '<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NS2ZHBQ7" height="0" width="0" style="display:none;visibility:hidden"></iframe>',
-          }}
-        />
-        {/* End Google Tag Manager (noscript) */}
         {children}
       </body>
     </html>
