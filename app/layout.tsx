6ea9de5bd60c09d3936213fcf04cd79e2c7233d2
SHIMOTORI Masanari <shimotori.masanari@gmail.com>
2025-10-03 00:33:55 +0900

    :test_tube: GTM導入

--- app/layout.tsx
+++ app/layout.tsx
@@ -67,7 +67,25 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html:
+              "(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer','GTM-NS2ZHBQ7');",
+          }}
+        />
+        {/* End Google Tag Manager */}
+      </head>
       <body className={`${notoSerifJp.className} ${notoSansJp.variable} ${sawarabiMincho.variable}`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html:
+              '<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NS2ZHBQ7" height="0" width="0" style="display:none;visibility:hidden"></iframe>',
+          }}
+        />
+        {/* End Google Tag Manager (noscript) */}
         {children}
       </body>
     </html>
